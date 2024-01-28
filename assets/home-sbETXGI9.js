8bbe426b4684ea9777dd7e90c959479ef0982e06
Anand raja <meetanandns@gmail.com>
2024-01-28 09:00:55 +0000

    Updates

---
+++ assets/home-sbETXGI9.js
@@ -0,0 +1,4 @@
+import"./modulepreload-polyfill-9p4a8sJU.js";const e=`<header>Header</header>
+<footer>Footer</footer>
+<main>Main Content</main>
+<aside>Sidebar</aside>`;document.querySelector("#app").innerHTML=e;
