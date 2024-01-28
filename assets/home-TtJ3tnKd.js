34387fdb0c16507455caf86fda2933a4ed89663a
Anand raja <meetanandns@gmail.com>
2024-01-28 16:36:35 +0000

    Updates

--- assets/home-TtJ3tnKd.js
+++ assets/home-TtJ3tnKd.js
@@ -1,4 +1,5 @@
 import"./modulepreload-polyfill-9p4a8sJU.js";const e=`<header>Header</header>
 <footer>Footer</footer>
 <main>Main Content</main>
-<aside>Sidebar</aside>`;document.querySelector("#app").innerHTML=e;
+<aside>Sidebar</aside>
+`;document.querySelector("#app").innerHTML=e;
