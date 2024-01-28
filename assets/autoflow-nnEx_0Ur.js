87f51d716f0b0cc92d9d1468a458131cfecdf91c
Anand raja <meetanandns@gmail.com>
2024-01-28 20:00:39 +0000

    Updates

--- assets/autoflow-nnEx_0Ur.js
+++ assets/autoflow-nnEx_0Ur.js
@@ -1,5 +1,5 @@
 import"./modulepreload-polyfill-9p4a8sJU.js";const e=`<div class="container">
-  <div class="quote quote--featured">Get all of Max' courses, they really are the best!</div>
+  <div class="quote quote--featured">Get all of AR's courses, they really are the best!</div>
   <div class="quote">Don't cry because it's over, smile because it happened.</div>
   <div class="quote">I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.</div>
   <div class="quote">Be yourself; everyone else is already taken.</div>
